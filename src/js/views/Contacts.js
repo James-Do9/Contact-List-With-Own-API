d4bfe26f2340fbc176241672320311bd823ffff7
James-Do9 <dojames3@yahoo.com>
2021-01-07 18:53:37 +0000

    Update

--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -6,11 +6,28 @@ import { Modal } from "../component/Modal";
 
 export const Contacts = () => {
 	const { store, actions } = useContext(GlobalState);
+
 	const [state, setState] = useState({
 		showModal: false,
 		id: null
 	});
+	const [search, setSearch] = useState("");
+	const [finalResults, setFinalResults] = useState([]);
 
+	const handleChange = e => {
+		setSearch(e.target.value);
+	};
+
+	useEffect(() => {
+		const data = store.agenda
+			? store.agenda.map((content, index) => {
+					return content.full_name;
+			  })
+			: null;
+		const userResults = data ? data.filter(data => data.toLowerCase().includes(search.toLowerCase())) : null;
+		setFinalResults(userResults);
+	}, [search]);
+	console.log(search);
 	return (
 		<div className="container">
 			<div>
@@ -19,7 +36,31 @@ export const Contacts = () => {
 						Add new contact
 					</Link>
 				</p>
+				<div>
+					<input
+						type="text"
+						value={search.toLowerCase()}
+						onChange={handleChange}
+						placeholder="Search for a contact..."></input>
+				</div>
 				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
+                    <ul className="list-group pull-down" id="contact-list">
+					{store.agenda
+						? store.agenda.map((contact, index) => {
+								if (finalResults == contact.full_name) {
+									return (
+										<ContactCard
+											key={index}
+											propContact={contact}
+											onDelete={() => setState({ showModal: true, id: contact.id })}
+										/>
+									);
+								}
+						  })
+						: null}
+                        </ul>
+				</div>
+				{/*<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
 					<ul className="list-group pull-down" id="contact-list">
 						{store.agenda
 							? store.agenda.map((contact, index) => {
@@ -35,9 +76,23 @@ export const Contacts = () => {
 							  })
 							: null}
 					</ul>
-				</div>
+                            </div>*/}
 			</div>
 			<Modal show={state.showModal} id={state.id} onClose={() => setState({ showModal: false })} />
 		</div>
 	);
 };
+
+/*{store.agenda
+						? store.agenda.map((contact, index) => {
+								if (finalResults == contact.full_name) {
+									return (
+										<ContactCard
+											key={index}
+											propContact={contact}
+											onDelete={() => setState({ showModal: true, id: contact.id })}
+										/>
+									);
+								}
+						  })
+						: null}*/
